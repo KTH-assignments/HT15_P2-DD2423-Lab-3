9f19dd53d4274643640ff7ec180d52732d9e388e
alexandros filotheou <alefil@kth.se>
2015-12-04 20:48:44 +0100

    Initial commit

---
+++ sol/my_functions/kmeans segm.m
@@ -0,0 +1,9 @@
+% Given an image,
+% the number of cluster centers K,
+% number of iterations L and
+% a seed for initializing randomization,
+% computes a segmentation (with a colour index per pixel) and the centers of all
+% clusters in 3D colour space.
+function [ segmentation, centers ] = kmeans segm(image, K, L, seed)
+
+end

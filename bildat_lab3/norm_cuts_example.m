43ea7b62571825da7af1b9c6797402c6220f32c6
alexandros filotheou <alefil@kth.se>
2015-12-10 00:06:55 +0100

    Up to Question 10

--- bildat_lab3/norm_cuts_example.m
+++ bildat_lab3/norm_cuts_example.m
@@ -1,4 +1,4 @@
-colour_bandwidth = 100.0; % color bandwidth
+colour_bandwidth = 20.0; % color bandwidth
 %radius = 3;              % maximum neighbourhood distance
 %ncuts_thresh = 0.2;      % cutting threshold
 %min_area = 200;          % minimum area of segment
@@ -6,17 +6,15 @@ colour_bandwidth = 100.0; % color bandwidth
 scale_factor = 0.4;      % image downscale factor
 image_sigma = 2.0;       % image preblurring scale
 
-radius = 3;              % maximum neighbourhood distance
+radius = 4;              % maximum neighbourhood distance
 ncuts_thresh = 0.5;      % cutting threshold
 min_area = 10;         % minimum area of segment
-max_depth = 8;           % maximum splitting depth
+max_depth = 10;           % maximum splitting depth
 
-
-
-I = imread('orange.jpg');
+%I = imread('orange.jpg');
 %I = imread('tiger1.jpg');
 %I = imread('tiger2.jpg');
-%I = imread('tiger3.jpg');
+I = imread('tiger3.jpg');
 
 
 I = imresize(I, scale_factor);

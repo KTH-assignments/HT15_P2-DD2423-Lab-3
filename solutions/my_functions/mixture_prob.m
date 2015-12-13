2aae34eaa195cc0418b6dafd25c130eb7c99a9ef
alexandros filotheou <alefil@kth.se>
2015-12-13 06:49:16 +0100

    The same

--- solutions/my_functions/mixture_prob.m
+++ solutions/my_functions/mixture_prob.m
@@ -22,8 +22,11 @@ function prob = mixture_prob(image, K, L, mask)
   % mu_k -> centers
   [segmentation centers] = kmeans_segm(image_masked, K, L, 1421);
 
-  centers
-
+%  centers_without_black = centers;
+  %blacks_idx = find(centers_without_black < 0.0001);
+  %centers_without_black(blacks_idx) = [];
+  %centers_without_black = reshape(centers_without_black, size(centers,1) - size(blacks_idx,1) / 3, 3);
+  %delete_idx = blacks_idx(1:size(blacks_idx,1)/3);
 
   % Reshape segmentation into one column
   segmentation = reshape(segmentation, size(segmentation,1) * size(segmentation,2), 1);
@@ -36,8 +39,12 @@ function prob = mixture_prob(image, K, L, mask)
   w = zeros(K, 1);
 
   for i = 1:K
-    w(i) = sum(segmentation(:,1) == i) + 1;
+    w(i) = (sum(segmentation(:,1) == i) + 1);
   end
+
+%  for i = 1:size(delete_idx,1)
+    %w(delete_idx(i)) = 1;
+  %end
 
   w = w / sum(w,1);
 
@@ -76,7 +83,11 @@ function prob = mixture_prob(image, K, L, mask)
 
 
     for kernel = 1:K
-      p(:,kernel) = nom_p(:,kernel) .* spfun(@(x) 1./(x), sum_denom_p);
+      %p(:,kernel) = nom_p(:,kernel) .* spfun(@(x) 1./(x), sum_denom_p);
+      p(:,kernel) = nom_p(:,kernel);% ./ sum_denom_p;
+
+      % Normalize?
+      %p(:,kernel) = p(:,kernel) / sum(p(:,kernel), 1);
     end
 
 
@@ -94,10 +105,15 @@ function prob = mixture_prob(image, K, L, mask)
 
       diff = bsxfun(@minus, image_masked_vec, centers(kernel, :));
 
-      d = p(:,kernel)' * diff * diff';
-      d = d';
-      d = d / sum(d,1);
-      cov{kernel} =  abs(sum(d,1)) / sum(p(:, kernel), 1) * eye(3);
+
+      %d = p(:,kernel)' * diff * diff';
+      d = diff' * (diff .* repmat(p(:,kernel), [1 3]));
+      %d = d';
+      %d = d / sum(d,1);
+      %cov{kernel} =  abs(sum(d,1)) / sum(p(:, kernel), 1) * eye(3);
+      diag_d = diag(diag(d));
+      cov{kernel} = diag_d / sum(p(:, kernel), 1) + 0.1 * eye(3);
+      %g(:,kernel) = g(:,kernel) / sum(g(:,kernel), 1);
     end
 
   end

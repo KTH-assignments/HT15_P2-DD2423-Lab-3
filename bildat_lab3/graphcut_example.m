4a25d0bdf74be25c8fcdffe8ff0ffacdc019526f
alexandros filotheou <alefil@kth.se>
2015-12-13 10:12:31 +0100

    The same

--- bildat_lab3/graphcut_example.m
+++ bildat_lab3/graphcut_example.m
@@ -4,11 +4,11 @@
 %alpha = 8.0;                 % maximum edge cost
 %sigma = 10.0;                % edge cost decay factor
 
-scale_factor = 0.5;          % image downscale factor
-area = [ 80, 110, 570, 300 ] % image region to train foreground with
-K = 16;                      % number of mixture components
-alpha = 8.0;                 % maximum edge cost
-sigma = 10.0;                % edge cost decay factor
+scale_factor = 0.5;           % image downscale factor
+area = [ 80, 110, 570, 300 ]; % image region to train foreground with
+K = 16;                       % number of mixture components
+alpha = 8.0;                  % maximum edge cost
+sigma = 10.0;                 % edge cost decay factor
 
 I = imread('tiger1.jpg');
 I = imresize(I, scale_factor);
--- solutions/my_functions/mixture_prob.m
+++ solutions/my_functions/mixture_prob.m
@@ -22,29 +22,19 @@ function prob = mixture_prob(image, K, L, mask)
   % mu_k -> centers
   [segmentation centers] = kmeans_segm(image_masked, K, L, 1421);
 
-%  centers_without_black = centers;
-  %blacks_idx = find(centers_without_black < 0.0001);
-  %centers_without_black(blacks_idx) = [];
-  %centers_without_black = reshape(centers_without_black, size(centers,1) - size(blacks_idx,1) / 3, 3);
-  %delete_idx = blacks_idx(1:size(blacks_idx,1)/3);
-
   % Reshape segmentation into one column
   segmentation = reshape(segmentation, size(segmentation,1) * size(segmentation,2), 1);
 
   % sigma_k
   cov = cell(K,1);
-  cov(:) = {eye(3)};
+  cov(:) = {100 * rand * eye(3) + ones(3,3)};
 
   % w_k
   w = zeros(K, 1);
 
   for i = 1:K
-    w(i) = (sum(segmentation(:,1) == i) + 1);
+    w(i) = sum(segmentation(:,1) == i) + 1;
   end
-
-%  for i = 1:size(delete_idx,1)
-    %w(delete_idx(i)) = 1;
-  %end
 
   w = w / sum(w,1);
 
@@ -65,9 +55,9 @@ function prob = mixture_prob(image, K, L, mask)
 % --------- Expectation: Compute probabilities P_ik using masked pixels --------
     for kernel = 1:K
 
-      diff = bsxfun(@minus, image_masked_vec, centers(kernel, :));
+      diff = abs(bsxfun(@minus, image_masked_vec, centers(kernel, :)));
 
-      g(:,kernel) = ((2 * pi)^3 * det(cov{kernel}))^(-1/2) * ...
+      g(:,kernel) = ((2 * pi)^3 * abs(det(cov{kernel})))^(-1/2) * ...
         exp(-0.5 * sum(diff * inv(cov{kernel}) .* diff, 2));
 
       % g should sum up to one
@@ -83,8 +73,8 @@ function prob = mixture_prob(image, K, L, mask)
 
 
     for kernel = 1:K
-      %p(:,kernel) = nom_p(:,kernel) .* spfun(@(x) 1./(x), sum_denom_p);
-      p(:,kernel) = nom_p(:,kernel);% ./ sum_denom_p;
+      p(:,kernel) = nom_p(:,kernel) ./ sum_denom_p;
+      %p(:,kernel) = nom_p(:,kernel);
 
       % Normalize?
       %p(:,kernel) = p(:,kernel) / sum(p(:,kernel), 1);
@@ -105,15 +95,12 @@ function prob = mixture_prob(image, K, L, mask)
 
       diff = bsxfun(@minus, image_masked_vec, centers(kernel, :));
 
-
-      %d = p(:,kernel)' * diff * diff';
       d = diff' * (diff .* repmat(p(:,kernel), [1 3]));
-      %d = d';
-      %d = d / sum(d,1);
       %cov{kernel} =  abs(sum(d,1)) / sum(p(:, kernel), 1) * eye(3);
       diag_d = diag(diag(d));
-      cov{kernel} = diag_d / sum(p(:, kernel), 1) + 0.1 * eye(3);
-      %g(:,kernel) = g(:,kernel) / sum(g(:,kernel), 1);
+      %cov{kernel} = (diag_d - 0.0000001 * eye(3)) / sum(p(:, kernel), 1); % works?
+      cov{kernel} = (diag_d + 0.0000001 * eye(3)) / sum(p(:, kernel), 1); % works?
+
     end
 
   end

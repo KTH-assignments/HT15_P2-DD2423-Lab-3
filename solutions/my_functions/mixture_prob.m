0db0f971a14882304e9309a3abf7615b48ffacce
alexandros filotheou <alefil@kth.se>
2015-12-14 14:56:20 +0100

    This is it

--- solutions/my_functions/mixture_prob.m
+++ solutions/my_functions/mixture_prob.m
@@ -5,48 +5,46 @@ function prob = mixture_prob(image, K, L, mask)
 
   N = height * width;
 
-  % Reshape image into 2D
-  image_vec = im2double(reshape(image, width * height, 3));
 
 % -------------- Store all pixels for which mask=1 in a Nx3 matrix -------------
 
-  % The image masked
-  image_masked = image .* repmat(mask, [1 1 3]);
+  % Reshape image into 2D
+  image_vec = im2double(reshape(image, N, 3));
 
-  % The masked image in 2D
-  image_masked_vec = im2double(reshape(image_masked, N, 3));
+  % Identify and obtain the masked pixels
+  image_masked_vec = image_vec(find(mask), :);
+
+  % The total number of masked pixels
+  M = size(image_masked_vec, 1);
 
 
 % ----------- Randomly initialize the K components using masked pixels ---------
 
   % mu_k -> centers
-  [segmentation centers] = kmeans_segm(image_masked, K, L, 1421);
-
-  % Reshape segmentation into one column
-  segmentation = reshape(segmentation, size(segmentation,1) * size(segmentation,2), 1);
+  [segmentation centers] = kmeans_segm_for_mixture(image_masked_vec, K, 10*L, 1421);
 
   % sigma_k
   cov = cell(K,1);
-  cov(:) = {100000 * (1 + rand) * eye(3) +  ones(3,3)};
+  cov(:) = {0.04 * rand * eye(3)};
 
   % w_k
   w = zeros(K, 1);
 
   for i = 1:K
-    w(i) = sum(segmentation(:,1) == i) + 1;
+    w(i) = sum(segmentation(:,1) == i);
   end
 
   w = w / sum(w,1);
 
 
   % Initialize p(i,k) for the masked pixels
-  p = zeros(N, K);
+  p = zeros(M, K);
 
   % Initialize g(i,k) for the masked pixels
-  g = zeros(N, K);
+  g = zeros(M, K);
 
   % The nominator of Step 1
-  nom_p = zeros(N, K);
+  nom_p = zeros(M, K);
 
 
 % ------------------------------- Iterate L times ------------------------------
@@ -56,9 +54,6 @@ function prob = mixture_prob(image, K, L, mask)
     for kernel = 1:K
 
       g(:,kernel) = g_k(image_masked_vec, centers(kernel,:), cov{kernel});
-
-      % g should sum up to one
-      g(:,kernel) = g(:,kernel) / sum(g(:,kernel), 1);
 
       nom_p(:,kernel) = w(kernel) * g(:, kernel);
 
@@ -76,29 +71,26 @@ function prob = mixture_prob(image, K, L, mask)
 
 % --- Maximization: Update weights, means and covariances using masked pixels --
     for kernel = 1:K
-      w(kernel) = sum(p(:, kernel), 1) / N;
+      w(kernel) = sum(p(:, kernel), 1) / M;
 
       centers(kernel, :) = p(:,kernel)' * image_masked_vec / (sum(p(:, kernel), 1));
     end
-
-    % Normalize the weights
-    w = w / sum(w,1);
 
     for kernel = 1:K
 
       diff = bsxfun(@minus, image_masked_vec, centers(kernel, :));
 
       d = diff' * (diff .* repmat(p(:,kernel), [1 3]));
+
       diag_d = diag(diag(d));
 
-      n = sum(p(:,kernel)' * (diff .* diff), 2);
-
-      cov{kernel} =  (n * eye(3) + diag_d + rand) / sum(p(:, kernel), 1);
-
+      cov{kernel} =  (d / sum(p(:, kernel), 1)) * eye(3) + 0.0001 * diag(diag(rand(3)));
     end
 
   end
 
+
+% ---------- Compute probabilities p(c_i) in Eq.(3) for all pixels I -----------
   pos_NK = zeros(N,K);
 
   for kernel = 1:K
